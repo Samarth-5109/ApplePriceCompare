f360fb4e3ddb97a79a51758437a43aa14836d34d
Samarth-5109 <samarthb5109@gmail.com>
2026-03-14 13:20:45 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-gray-100">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
